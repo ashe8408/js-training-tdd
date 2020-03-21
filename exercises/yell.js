d6f4fc7d677c7d1b1c14d699d94580e0e90a4dbc
ashe8408 <anael.b.d@hotmail.fr>
2020-03-21 12:44:35 +0100

    yell.js solved

--- exercises/yell.js
+++ exercises/yell.js
@@ -7,9 +7,17 @@
  */
 
 // Your code:
-
+function yell(str) {
+    return str.toUpperCase();
+}
 //* Begin of tests
 const assert = require('assert');
 
-assert.fail('You must write your own tests');
+//assert.fail('You must write your own tests');
+assert.strictEqual(typeof yell, 'function');
+assert.strictEqual(yell.length, 1);
+assert.strictEqual(yell('Hello World'), 'HELLO WORLD');
+assert.strictEqual(yell(''), '');
+assert.strictEqual(yell('Hello World'), 'HELLO WORLD');
+assert.strictEqual(yell('i wrote my own tests'), 'I WROTE MY OWN TESTS');
 // End of tests */
